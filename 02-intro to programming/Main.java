6a488defd3814e9ac343bfa298a0cf9b88951da9
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-09-11 04:37:08 +0530

    gitignore changed

--- 02-intro to programming/Main.java
+++ 02-intro to programming/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
         System.out.println("Hello World!");
+        System.out.println(age);
     }
 }
