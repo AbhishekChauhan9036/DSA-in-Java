e6d57677dcbb64a5534fe562af857a552a521959
abhishekchauhan9036 <abhishekchauhan9036@gmail.com>
2024-09-28 20:31:21 +0530

    intro to programming

--- 02-intro to programming/Main.java
+++ 02-intro to programming/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
         System.out.println("Hello World!");
